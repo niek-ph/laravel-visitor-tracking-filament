8e32701f016024fc8d85b8dab7e0b9e15e51429f
Niek Poorthuis <niek@appcenternederland.nl>
2025-09-24 11:44:23 +0200

    - cleanup

--- src/Filament/Widgets/VisitorCountTodayWidget.php
+++ src/Filament/Widgets/VisitorCountTodayWidget.php
@@ -17,23 +17,32 @@ class VisitorCountTodayWidget extends StatsOverviewWidget
 
         return [
             Stat::make(__('visitor-tracking-filament::widgets.visitor_counts.visitors_total'),
-                Number::forHumans($data['visitors_total'], 2, 2, true)),
+                $this->formatNumber($data['visitors_total'])),
             Stat::make(__('visitor-tracking-filament::widgets.visitor_counts.visitors_today'),
-                Number::forHumans($data['visitors_today'], 2, 2, true)),
+                $this->formatNumber($data['visitors_today'])),
             Stat::make(__('visitor-tracking-filament::widgets.visitor_counts.page_views_today'),
-                Number::forHumans($data['page_views_today'], 2, 2, true)),
+                $this->formatNumber($data['page_views_today'])),
         ];
+    }
+
+    private function formatNumber(int $number): string
+    {
+        return Number::forHumans($number, 2, 2, true);
     }
 
     private function getData(): array
     {
+        $today = today();
+
         return [
             'visitors_total' => VisitorTracking::$visitorModel::count(),
-            'visitors_today' => VisitorTracking::$visitorModel::whereHas('events', function ($q) {
-                $q->whereDate('created_at', today());
-            })->count(),
-            'page_views_today' => VisitorTracking::$eventModel::where('name', 'page_view')->whereDate('created_at',
-                today())->count(),
+            'visitors_today' => VisitorTracking::$visitorModel::whereHas(
+                'events',
+                fn ($q) => $q->whereDate('created_at', $today)
+            )->count(),
+            'page_views_today' => VisitorTracking::$eventModel::where('name', 'page_view')
+                ->whereDate('created_at', $today)
+                ->count(),
         ];
     }
 }
